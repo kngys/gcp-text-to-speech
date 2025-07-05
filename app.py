9ce33695e89494ab72c280a574b79bfb42f1f73f
kngys <kristiangys@gmail.com>
2025-07-05 23:49:49 +0800

    Add function to generate audio from ssml

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import html
 
 def synthesize_text():
     """
-    Synthesises audio from plaintext.
+    Synthesizes audio from plaintext.
     """
 
     client = texttospeech.TextToSpeechClient.from_service_account_file("gcp_key.json")
@@ -41,21 +41,32 @@ def text_to_ssml(input_file: str) -> str:
 
     return ssml
 
+def synthesize_ssml(ssml_file_path: str):
+    """
+    Synthesizes audio from ssml.
+    """
 
+    with open(ssml_file_path) as f:
+        ssml_input = f.read()
 
+    client = texttospeech.TextToSpeechClient.from_service_account_file("gcp_key.json")
+    synthesis_input = texttospeech.SynthesisInput(ssml=ssml_input)
 
+    voice = texttospeech.VoiceSelectionParams(
+            language_code="en-US", name="en-US-Neural2-I"
+    )
+    audio_config = texttospeech.AudioConfig(
+            audio_encoding=texttospeech.AudioEncoding.MP3,
+            speaking_rate=1.0
+    )
 
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
 
-
-
-
-
-
-    
-
-
-
-
+    with open("ssml_output.mp3", "wb") as out:
+        out.write(response.audio_content)
+        print('Audio content written to file "ssml_output.mp3"')
 
 if __name__ == "__main__":
-    synthesize()
+    synthesize_ssml("text_input.ssml")
