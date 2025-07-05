09483b99419be5d6464fa63057d9b528a23efc84
kngys <kristiangys@gmail.com>
2025-07-05 18:24:20 +0800

    Add function to convert plaintext to ssml

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from google.cloud import texttospeech
+import html
 
-def synthesize():
+def synthesize_text():
+    """
+    Synthesises audio from plaintext.
+    """
+
     client = texttospeech.TextToSpeechClient.from_service_account_file("gcp_key.json")
     synthesis_input = texttospeech.SynthesisInput(text="Hello, world!")
     voice = texttospeech.VoiceSelectionParams(
@@ -17,6 +22,40 @@ def synthesize():
     with open("output.mp3", "wb") as out:
         out.write(response.audio_content)
         print('Audio content written to file "output.mp3"')
+
+def text_to_ssml(input_file: str) -> str:
+    """
+    Converts plaintext to Speech Synthesis Markup Language (SSML).
+    """
+
+    with open(input_file) as f:
+        raw_lines = f.read()
+
+    # Replace HTML special characters 
+    escaped_lines = html.escape(raw_lines)
+
+    # Convert plaintext to SSML
+    ssml = "<speak>{}</speak>".format(
+        escaped_lines.replace("\n", '\n<break time="2s"/>')
+    )
+
+    return ssml
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
 
 if __name__ == "__main__":
     synthesize()
