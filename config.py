c4b7bd78b1c86fd9f3a1c1a9fbfc61a21010d889
kngys <kristiangys@gmail.com>
2025-07-15 23:29:44 +0800

    Add config.py to handle saving of authentication key

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+import json
+
+CONFIG_FILE = "config.json"
+
+def load_config():
+    if os.path.exists(CONFIG_FILE):
+        with open(CONFIG_FILE) as f:
+            return json.load(f)
+    return {}
+
+def save_config(data):
+    with open(CONFIG_FILE, "w") as f:
+        json.dump(data, f)
